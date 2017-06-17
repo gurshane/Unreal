d926cb74ea85856997273d0e9bba5822d65a601b
gurshane <gurshane.sidhu@mail.mcgill.ca>
2017-06-16 21:08:34 -0400

    continued work on ai

--- Unreal/Bellz/Source/Bellz/Enemy.cpp
+++ Unreal/Bellz/Source/Bellz/Enemy.cpp
@@ -2,7 +2,9 @@
 
 #include "Bellz.h"
 #include "Enemy.h"
-
+#include "EnemyAIController.h"
+#include "Gladiator.h"
+#include "Perception/PawnSensingComponent.h"
 
 // Sets default values
 AEnemy::AEnemy()
@@ -10,6 +12,47 @@ AEnemy::AEnemy()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	AIControllerClass = AEnemyAIController::StaticClass();
+
+	TotalHealth = 100;
+	AttackRange = 100;
+	AttackDamage = 10;
+	IsDead = false;
+	IsAttacking = false;
+
+	bUseControllerRotationYaw = true;
+
+	//setup pawn sensor
+	PawnSensor = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("Pawn Sensor"));
+	PawnSensor->SensingInterval = .25f;
+	PawnSensor->bOnlySensePlayers = true; //wont sense other ai players
+	PawnSensor->SetPeripheralVisionAngle(85.0f);
+
+	//setup body trigger
+	bodySphereTrigger = CreateDefaultSubobject<USphereComponent>(TEXT("BodyTriggerSphere"));
+	bodySphereTrigger->SetSphereRadius(150.0f);
+	bodySphereTrigger->AttachTo(Mesh);
+
+	//color of hand trigger
+	FColor handsTriggersColor = FColor(0, 0, 255, 255);
+
+	//setup and attach left hand trigger
+	leftHandTrigger = CreateDefaultSubobject<USphereComponent>(TEXT("LeftHandTrigger"));
+	leftHandTrigger->SetSphereRadius(70.0f);
+	leftHandTrigger->AttachTo(Mesh, "hand_lf", EAttachLocation::SnapToTarget);
+	leftHandTrigger->ShapeColor = handsTriggersColor;
+	leftHandTrigger->bGenerateOverlapEvents = 0;
+
+	//setup and attach right hand trigger
+	rightHandTrigger = CreateDefaultSubobject<USphereComponent>(TEXT("RightHandTrigger"));
+	rightHandTrigger->SetSphereRadius(70.0f);
+	rightHandTrigger->AttachTo(Mesh, "hand_rt", EAttachLocation::SnapToTarget);
+	rightHandTrigger->ShapeColor = handsTriggersColor;
+	rightHandTrigger->bGenerateOverlapEvents = 0;
+
+	//nav agent properties
+	GetCharacterMovement()->NavAgentProps.AgentRadius = 400.0f;
+	GetCharacterMovement()->SetUpdateNavAgentWithOwnersCollisions(false);
 }
 
 // Called when the game starts or when spawned
--- Unreal/Bellz/Source/Bellz/Enemy.h
+++ Unreal/Bellz/Source/Bellz/Enemy.h
@@ -23,6 +23,59 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
-	
-	
+	//Body trigger to take damage
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Triggers, meta = (AllowPrivateAccess = "true"))
+	class USphereComponent* bodySphereTrigger;
+
+	//Left hand trigger to deal damage
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Triggers, meta = (AllowPrivateAccess = "true"))
+	class USphereComponent* leftHandTrigger;
+
+	//Right hand trigger to deal damage
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Triggers, meta = (AllowPrivateAccess = "true"))
+	class USphereComponent* rightHandTrigger;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Behavior")
+	float TotalHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Behavior")
+	float AttackRange;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Behavior")
+	float AttackDamage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Behavior")
+	bool IsDead;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Behavior")
+	bool IsAttacking;
+
+	//Sensing component allows the AI to hear and see the player
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Enemy AI")
+	class UPawnSensingComponent* PawnSensor;
+
+	//Reference to the behavior tree the ai is going to use
+	UPROPERTY(EditAnywhere, Category = "EnemyAI")
+	class UBehaviorTree* EnemyBehaviorTree;
+
+	//Before, During, and After the attack
+	UFUNCTION(BlueprintCallable, Category = "Enemy AI")
+	void OnPreAttack();
+
+	UFUNCTION(BlueprintCallable, Category = "Enemy AI")
+	void OnPerformAttack();
+
+	UFUNCTION(BlueprintCallable, Category = "Enemy AI")
+	void OnPostAttack();
+
+	UFUNCTION()
+	void OnHearNoise(APawn* OtherActor, const FVector &Location, float Volume);
+
+	UFUNCTION()
+	void OnSeePawn(APawn* OtherPawn);
+
+	UFUNCTION()
+	void OnHandTriggerOverlap(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	FORCEINLINE class USphereComponent* GetBodySphereTrigger() const { return bodySphereTrigger; }
 };
